a3e76641bb085a17f5b2cb51c437aa70700dc421
danielostling <example@example.org>
2023-01-31 23:34:14 +0100

    Solution to part 1 of day 9, maybe. There might be a bug in this one.

--- day-9/day-9.lisp
+++ day-9/day-9.lisp
@@ -15,30 +15,83 @@
     (collect (list (coerce dir-str 'character) (parse-integer count-str)))))
 
 
-(defun test-step-sequence ()
-  "Test if given step sequence generates visited position list given in puzzle
-   test case."
-  (let ((step-sequence '((#\R 4) (#\U 4) (#\L 3) (#\D 1)
-                         (#\R 4) (#\D 1) (#\L 5) (#\R 2))))
-    ;; Fill in code here..
-    (test-visited-positions step-sequence)))
+(defun calculate-tail-move (head-pos tail-pos)
+  "Based on `head-pos` (x y) and `tail-pos` (i j), figure out where
+   to move tail. If no change is needed, return `tail-pos`.
+   This is a pretty naïve solution, I'm afraid but it gets the job done."
+  (flet ((one-step-behind (i)
+           "Return 1 if `i` is negative integer, else -1."
+           (if (minusp i) 1 -1)))
+    (let* ((head-x (first head-pos))
+           (head-y (second head-pos))
+           (tail-x (first tail-pos))
+           (tail-y (second tail-pos))
+           (distance-x (abs (- head-x tail-x)))
+           (distance-y (abs (- head-y tail-y))))
+
+ ;;; Cases
+ ;;;   6 4 6
+ ;;; 7 1 1 1 7
+ ;;; 3 1 X 1 2
+ ;;; 7 1 1 1 7
+ ;;;   6 5 6
+      (cond ((and (<= distance-x 1) (<= distance-y 1)) tail-pos) ;; case 1
+            ((and (= head-y tail-y) (> head-x tail-x))           ;; case 2
+             (list (1- head-x) tail-y)) 
+            ((and (= head-y tail-y) (< head-x tail-x))           ;; case 3
+             (list (1+ head-x) tail-y))
+            ((and (= head-x tail-x) (> head-y tail-y))           ;; case 4
+             (list tail-x (1- head-y)))
+            ((and (= head-x tail-x) (< head-y tail-y))           ;; case 5
+             (list tail-x (1+ head-y)))
+            ((= distance-y 2)                                    ;; case 6
+             (list head-x (+ head-y (one-step-behind (- head-y tail-y)))))
+            ((= distance-x 2)                                    ;; case 7
+             (list (+ head-x (one-step-behind (- head-x tail-x))) head-y))))))
+
+
+(defun walk-steps (steps)
+  "Go through the steps in the rope simulation. `steps` is a list of
+  (direction stepcount) pairs, where direction is one of #\U #\L #\R #\D and
+  stepcount is a positive integer. Collect all positions tail part of rope
+  passes and return the (unique) list of (x y) positions, where x and y are
+  integers. Start out with head and tail at same position, (0 0), which is in
+  the lower left corner of a coordinate system."
+  (let* ((head-pos '(0 0))
+         (tail-pos '(0 0))
+         (visited-positions '((0 0))))
+    (iter
+      (for (direction stepcount) in steps)
+      (iter
+        (for steps-taken from 1 to stepcount)
+        (for (head-x head-y) = head-pos)
+        (cond ((char-equal direction #\L) (setq head-pos (list (1- head-x) head-y)))
+              ((char-equal direction #\U) (setq head-pos (list head-x (1+ head-y))))
+              ((char-equal direction #\R) (setq head-pos (list (1+ head-x) head-y)))
+              ((char-equal direction #\D) (setq head-pos (list head-x (1- head-y)))))
+        (setq tail-pos (calculate-tail-move head-pos tail-pos))
+        (pushnew tail-pos visited-positions :test #'equal)))
+    visited-positions))
+
+
+(defun compare-positions (pos1 pos2)
+  "Compare `pos1` to `pos2`.
+   Return true if and only if the first argument is strictly less
+   than the second (in some appropriate sense). If the first argument
+   is greater than or equal to the second (in the appropriate sense),
+   then return false."
+  (cond ((< (first pos1) (first pos2)) t)
+        ((> (first pos1) (first pos2)) NIL)
+        (t (< (second pos1) (second pos2)))))
+
 
 (defun position-list-equal-p (position-list1 position-list2)
   "Return T if `position-list1` and `position-list2` are equal, else NIL.
    `position-list1` and `position-list2` are lists of positions,
    ((x1 y1) (x2 y2) ... (xn yn)), where x1..n and y1..n are integers."
-  (flet ((compare-positions (pos1 pos2)
-            "Compare `pos1` to `pos2`.
-             Return true if and only if the first argument is strictly less
-             than the second (in some appropriate sense). If the first argument
-             is greater than or equal to the second (in the appropriate sense),
-             then return false."
-           (cond ((< (first pos1) (first pos2)) t)
-                 ((> (first pos1) (first pos2)) NIL)
-                 (t (< (second pos1) (second pos2))))))
-    (let ((sorted-positions1 (sort (copy-seq position-list1) #'compare-positions))
-          (sorted-positions2 (sort (copy-seq position-list2) #'compare-positions)))
-      (equal sorted-positions1 sorted-positions2))))
+  (let ((sorted-positions1 (sort (copy-seq position-list1) #'compare-positions))
+        (sorted-positions2 (sort (copy-seq position-list2) #'compare-positions)))
+    (equal sorted-positions1 sorted-positions2)))
 
 
 (defun test-visited-positions (visited-positions)
@@ -63,13 +116,20 @@
                                (4 1) (1 2) (2 2) (3 2)
                                (4 2) (3 3) (4 3) (2 4)
                                (3 4))))
-    (equal test-case-positions visited-positions)))
+    (position-list-equal-p test-case-positions visited-positions)))
+
+
+(defun test-step-sequence ()
+  "Test if given step sequence generates visited position list given in puzzle
+   test case."
+  (let ((step-sequence '((#\R 4) (#\U 4) (#\L 3) (#\D 1)
+                          (#\R 4) (#\D 1) (#\L 5) (#\R 2))))
+    (test-visited-positions (walk-steps step-sequence))))
 
 
 (defun solve-part-1 (parsed-steps)
   "Solve part 1."
-  (declare (ignorable parsed-steps))
-  "Part 1 TBD.")
+  (length (walk-steps parsed-steps)))
 
 
 (defun solve-part-2 (parsed-steps)
