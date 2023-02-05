5574b9ae55835a22be454f88df2998f45683c1fc
danielostling <example@example.org>
2023-02-05 21:09:42 +0100

    Solution part 1 for day 10, with tests.

--- day-10/day-10.lisp
+++ day-10/day-10.lisp
@@ -33,7 +33,9 @@
 (defun run-program (instructions)
   "Run program consisting of `instructions`.
    Return list of X register integer value per cycle, where first value is
-   cycle 1. Not happy with this implementation, should be done entirely in iter."
+   cycle 1. Not happy with this implementation, as the final value is tacked on
+   outside the iteration. The whole thing should have been done in the iteration
+   itself instead."
   (let* ((register-x-value 1)
          (register-x-per-clock
            (iter outer
@@ -44,6 +46,29 @@
                    (collect register-x-value)))
              (setq register-x-value (+ register-x-value register-adjust)))))
     (append register-x-per-clock (list register-x-value))))
+
+
+(defun build-image (instructions)
+  "Use the register value and CRT beam position to build an image as a list of
+   strings. See day 10 part 2 for problem description for now."
+  (flet ((pixel-overlap-p (crt-x pixel-center)
+           "Determine if pixel covers CRT beam X position. Return T if it does,
+            else NIL."
+           (let ((pixel-xs (list (1- pixel-center) pixel-center (1+ pixel-center))))
+             (member crt-x pixel-xs :test #'=))))
+    (let ((pixel-center-per-clock (run-program instructions))
+          (line-width 40)
+          (line-count 6))
+      (iter 
+        (for line-idx from 0 below line-count)
+        (for line = (iter
+                      (for crt-x from 0 below line-width)
+                      (for beam-idx = (+ crt-x (* line-idx line-width)))
+                      (for pixel-center = (nth beam-idx pixel-center-per-clock))
+                      (if (pixel-overlap-p crt-x pixel-center)
+                          (collect "#")
+                          (collect "."))))
+        (collect (format nil "~{~a ~}" line))))))
 
 
 (defun compare-pairs (pair1 pair2)
@@ -81,6 +106,49 @@
                    (sort (copy-seq actual-result) #'>)))))
 
 
+(defun test-program2 ()
+  "Test program from day-10, second part."
+  (let* ((instructions '("addx 15" "addx -11" "addx 6" "addx -3" "addx 5"
+                         "addx -1" "addx -8" "addx 13" "addx 4" "noop" "addx -1"
+                         "addx 5" "addx -1" "addx 5" "addx -1" "addx 5"
+                         "addx -1" "addx 5" "addx -1" "addx -35" "addx 1"
+                         "addx 24" "addx -19" "addx 1" "addx 16" "addx -11"
+                         "noop" "noop" "addx 21" "addx -15" "noop" "noop"
+                         "addx -3" "addx 9" "addx 1" "addx -3" "addx 8" "addx 1"
+                         "addx 5" "noop" "noop" "noop" "noop" "noop" "addx -36"
+                         "noop" "addx 1" "addx 7" "noop" "noop" "noop" "addx 2"
+                         "addx 6" "noop" "noop" "noop" "noop" "noop" "addx 1"
+                         "noop" "noop" "addx 7" "addx 1" "noop" "addx -13"
+                         "addx 13" "addx 7" "noop" "addx 1" "addx -33" "noop"
+                         "noop" "noop" "addx 2" "noop" "noop" "noop" "addx 8"
+                         "noop" "addx -1" "addx 2" "addx 1" "noop" "addx 17"
+                         "addx -9" "addx 1" "addx 1" "addx -3" "addx 11" "noop"
+                         "noop" "addx 1" "noop" "addx 1" "noop" "noop"
+                         "addx -13" "addx -19" "addx 1" "addx 3" "addx 26"
+                         "addx -30" "addx 12" "addx -1" "addx 3" "addx 1" "noop"
+                         "noop" "noop" "addx -9" "addx 18" "addx 1" "addx 2"
+                         "noop" "noop" "addx 9" "noop" "noop" "noop" "addx -1"
+                         "addx 2" "addx -37" "addx 1" "addx 3" "noop" "addx 15"
+                         "addx -21" "addx 22" "addx -6" "addx 1" "noop" "addx 2"
+                         "addx 1" "noop" "addx -10" "noop" "noop" "addx 20"
+                         "addx 1" "addx 2" "addx 2" "addx -6" "addx -11" "noop"
+                         "noop" "noop"))
+         (expected-result '("##..##..##..##..##..##..##..##..##..##.."
+                            "###...###...###...###...###...###...###."
+                            "####....####....####....####....####...."
+                            "#####.....#####.....#####.....#####....."
+                            "######......######......######......####"
+                            "#######.......#######.......#######....."))
+         (actual-result (build-image  (parse-instructions instructions))))
+    (format t (concatenate 'string
+                           "Expected: ~%~a~%"
+                           "----------~%"
+                           "  Actual: ~%~a~%")
+            expected-result
+            actual-result)))
+
+
+
 (defun solve-part-1 (instructions)
   "Solve part 1."
   (let ((register-value-by-clock (run-program instructions)))
@@ -91,8 +159,7 @@
 
 (defun solve-part-2 (instructions)
   "Solve part 2."
-  (declare (ignorable instructions))
-  "TBD")
+  (build-image instructions))
 
 
 (defun main ()
@@ -102,4 +169,4 @@
          (part-1 (solve-part-1 instructions))
          (part-2 (solve-part-2 instructions)))
     (format t "First part: ~a~%" part-1)
-    (format t "Second part: ~a~%" part-2)))
+    (format t "Second part: ~%~a~%" part-2)))
